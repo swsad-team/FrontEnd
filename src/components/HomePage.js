607e42308b82de2db10182d266dcbf84cfdf8ceb
Dmaxzj <Dmaxzj@users.noreply.github.com>
2019-04-02 00:25:36 +0800

    add basic router

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>Home Page</h1>
+      </section>)
+  }
+}
+
+export default HomePage
