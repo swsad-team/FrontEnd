21bb2ba1c762eff24e20c01f1db3e432d5d57e30
Dmaxzj <260073361@qq.com>
2019-05-31 17:05:27 +0800

    添加用户登陆的 context

---
+++ src/context/index.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from 'react'
+
+export const UserContext = createContext({})
+
+export const UserProvider = props => {
+  const { login: initLogin, nickname: initNickname, children } = props
+  const [login, setLogin] = useState(initLogin)
+  const [nickname, setNickname] = useState(initNickname)
+
+  const userContext = {
+    login,
+    setLogin,
+    nickname,
+    setNickname
+  }
+  return (
+    <UserContext.Provider value={userContext}>{children}</UserContext.Provider>
+  )
+}
