d0cbe4e27919745b78297545c292a3936918cb09
Dmaxzj <260073361@qq.com>
2019-06-13 23:00:06 +0800

    添加新建问卷

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,83 +1,14 @@
-import React, { useState, forwardRef } from 'react'
-import {
-  Card,
-  Form,
-  InputNumber,
-  Input,
-  DatePicker,
-  Button,
-  Alert,
-  Radio,
-  Steps,
-  List,
-  Divider
-} from 'antd'
-import moment from 'moment'
+import React, { useState } from 'react'
+import { Card, Form, Input, Button, Steps, Divider, message } from 'antd'
+
 import './NewTask.css'
-import CheckAllBox from './CheckAllBox'
+
 import TaskDetails from './TaskDetails'
+import CreateSurvey, { SurveyList } from './CreateSurvey'
+import WrappedTaskBasicForm from './TaskBasicForm'
 
 const { TextArea } = Input
 const { Step } = Steps
-
-let LimitForm = ({ size, value = [], onChange }, ref) => {
-  const options = ['男', '女', '其他']
-  const [limits, setLimits] = useState(value)
-  const [checkedList, setCheckedList] = useState([])
-  const [grade, setGrade] = useState(null)
-  const handleClick = () => {
-    const newLimit = { gender: checkedList, grade: grade }
-    if (!(newLimit.gender.length || grade)) {
-      return
-    }
-    const flag = limits.find(item => {
-      return (
-        item.gender.toString() === newLimit.gender.toString() &&
-        item.grade === newLimit.grade
-      )
-    })
-    if (!flag && onChange) {
-      onChange(limits.concat(newLimit))
-      setLimits(limits.concat(newLimit))
-      setCheckedList([])
-      setGrade(null)
-    }
-  }
-
-  return (
-    <div ref={ref}>
-      <span>
-        <label>性别:</label>
-        <CheckAllBox
-          withCheckAll={true}
-          options={options}
-          checkedList={checkedList}
-          onChange={setCheckedList}
-        />
-        <label>学号前缀:</label>
-        <InputNumber
-          min={1}
-          value={grade}
-          max={Math.pow(10, 10) - 1}
-          onChange={setGrade}
-        />
-        <Button onClick={handleClick}>添加</Button>
-      </span>
-      {limits.length !== 0 && (
-        <List
-          dataSource={limits}
-          renderItem={item => (
-            <List.Item>
-              性别:{item.gender.join('/')} 学号前缀:{item.grade}{' '}
-            </List.Item>
-          )}
-        />
-      )}
-    </div>
-  )
-}
-
-LimitForm = forwardRef(LimitForm)
 
 const CommonTaskDetialForm = props => {
   const { onSubmit, formValues } = props
@@ -125,145 +56,11 @@ const WrappedCommonTaskDetialForm = Form.create({ name: 'task_basic' })(
   CommonTaskDetialForm
 )
 
-const TaskBasicForm = props => {
-  const { onSubmit, formValues } = props
-  const [tipTime, setTipTime] = useState('')
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    props.form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        // TODO: connet register api
-        console.log('Received values of form: ', values)
-        onSubmit(values)
-      }
-    })
-  }
-
-  const disabledDate = current => {
-    return current && current < moment().subtract('seconds', moment().seconds())
-  }
-
-  const handleDueTimeChange = _moment => {
-    if (!_moment) {
-      setTipTime('')
-    } else {
-      setTipTime(`任务将${_moment.fromNow(true)}后结束`)
-    }
-  }
-  const commonFields = [
-    {
-      type: 'title',
-      options: {
-        rules: [
-          {
-            min: 5,
-            max: 30,
-            required: true,
-            message: '输入5-30字的标题'
-          }
-        ]
-      }
-    },
-    {
-      type: 'isSurvey',
-      options: {
-        initialValue: false
-      }
-    },
-    {
-      type: 'dueTime',
-      options: {
-        rules: [
-          {
-            required: true,
-            message: '请输入截至日期'
-          }
-        ]
-      }
-    },
-    {
-      type: 'reward',
-      options: {
-        initialValue: 1
-      }
-    },
-    {
-      type: 'maximumParticipators',
-      options: {
-        initialValue: 1
-      }
-    },
-    {
-      type: 'limits',
-      options: {}
-    }
-  ]
-
-  commonFields.forEach(item => {
-    if (item.type in formValues) {
-      item.options.initialValue = formValues[item.type]
-    }
-  })
-
-  const { getFieldDecorator } = props.form
-  const commonDecorators = {}
-  commonFields.forEach(field => {
-    commonDecorators[field.type] = getFieldDecorator(field.type, field.options)
-  })
-
-  return (
-    <Card title="任务" className="task-card">
-      <Form onSubmit={handleSubmit} className="login-form">
-        <Form.Item label="标题">
-          {commonDecorators['title'](<Input type="text" />)}
-        </Form.Item>
-        <Form.Item label="任务类型">
-          {commonDecorators['isSurvey'](
-            <Radio.Group>
-              <Radio value={false}>普通任务</Radio>
-              <Radio value={true}>问卷</Radio>
-            </Radio.Group>
-          )}
-        </Form.Item>
-        <Form.Item label="截至时间">
-          {commonDecorators['dueTime'](
-            <DatePicker
-              format="YYYY-MM-DD HH:mm"
-              showTime={{
-                format: 'HH:mm'
-              }}
-              disabledDate={disabledDate}
-              onChange={handleDueTimeChange}
-            />
-          )}
-          {tipTime && <Alert message={tipTime} type="info" showIcon />}
-        </Form.Item>
-        <Form.Item label="悬赏">
-          {commonDecorators['reward'](<InputNumber min={1} suffix="金币" />)}
-        </Form.Item>
-        <Form.Item label="最多参与人数">
-          {commonDecorators['maximumParticipators'](<InputNumber min={1} />)}
-        </Form.Item>
-        <Form.Item label="额外限制">
-          {commonDecorators['limits'](<LimitForm />)}
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            继续填写
-          </Button>
-        </Form.Item>
-      </Form>
-    </Card>
-  )
-}
-
-const WrappedTaskBasicForm = Form.create({ name: 'task_basic' })(TaskBasicForm)
-
 const NewTaskPage = props => {
   const [basicValues, setBasicValues] = useState({})
   const [commonTaskDetialValues, setCommonTaskDetialValues] = useState({})
   const [currentStep, setCurrentStep] = useState(0)
+  const [surveyData, setSurveyData] = useState([])
 
   const prev = e => {
     setCurrentStep(currentStep - 1)
@@ -287,6 +84,28 @@ const NewTaskPage = props => {
     } else {
       prev()
     }
+  }
+
+  const handleAddQuestion = value => {
+    console.log(surveyData)
+    if (surveyData.some(item => item.title === value.title)) {
+      message.error('问题已存在')
+      return false
+    } else {
+      message.success('创建问题成功')
+      setSurveyData(surveyData.concat(value))
+      return true
+    }
+  }
+
+  const handleRemoveQuestion = index => {
+    setSurveyData(surveyData.filter((item, _index) => _index !== index))
+  }
+
+  const handleSurveySubmit = () => {
+    // TODO:
+    console.log(surveyData)
+    next()
   }
 
   const typeMap = {
@@ -315,7 +134,15 @@ const NewTaskPage = props => {
     },
     {
       title: '填写任务详情',
-      content: (
+      content: basicValues.isSurvey ? (
+        <CreateSurvey
+          onSubmit={handleSurveySubmit}
+          onPrev={() => prev()}
+          onChange={handleAddQuestion}
+          onRemove={handleRemoveQuestion}
+          surveyData={surveyData}
+        />
+      ) : (
         <WrappedCommonTaskDetialForm
           formValues={commonTaskDetialValues}
           onSubmit={handleCommonDetialSubmit}
@@ -330,7 +157,11 @@ const NewTaskPage = props => {
           <Divider orientation="left">基本信息</Divider>
           <TaskDetails values={basicValues} typeMap={typeMap} />
           <Divider orientation="left">详细信息</Divider>
-          <TaskDetails values={commonTaskDetialValues} typeMap={typeMap} />
+          {basicValues.isSurvey ? (
+            <SurveyList dataSource={surveyData} />
+          ) : (
+            <TaskDetails values={commonTaskDetialValues} typeMap={typeMap} />
+          )}
           <Button>返回首页</Button>
         </Card>
       )
---
+++ src/components/TaskBasicForm.js
@@ -0,0 +1,197 @@
+import React, { useState, forwardRef } from 'react'
+import { Form, Button, Card, Radio, DatePicker, InputNumber, Alert, Input, List } from 'antd'
+
+import moment from 'moment'
+import CheckAllBox from './CheckAllBox';
+
+let LimitForm = ({ value = [], onChange }, ref) => {
+  const options = ['男', '女', '其他']
+  const [limits, setLimits] = useState(value)
+  const [checkedList, setCheckedList] = useState([])
+  const [grade, setGrade] = useState(null)
+  const handleClick = () => {
+    const newLimit = { gender: checkedList, grade: grade }
+    if (!(newLimit.gender.length || grade)) {
+      return
+    }
+    const flag = limits.find(item => {
+      return (
+        item.gender.toString() === newLimit.gender.toString() &&
+        item.grade === newLimit.grade
+      )
+    })
+    if (!flag && onChange) {
+      onChange(limits.concat(newLimit))
+      setLimits(limits.concat(newLimit))
+      setCheckedList([])
+      setGrade(null)
+    }
+  }
+
+  return (
+    <div ref={ref}>
+      <span>
+        <label>性别:</label>
+        <CheckAllBox
+          withCheckAll={true}
+          options={options}
+          checkedList={checkedList}
+          onChange={setCheckedList}
+        />
+        <label>学号前缀:</label>
+        <InputNumber
+          min={1}
+          value={grade}
+          max={Math.pow(10, 10) - 1}
+          onChange={setGrade}
+        />
+        <Button onClick={handleClick}>添加</Button>
+      </span>
+      {limits.length !== 0 && (
+        <List
+          dataSource={limits}
+          renderItem={item => (
+            <List.Item>
+              性别:{item.gender.join('/')} 学号前缀:{item.grade}{' '}
+            </List.Item>
+          )}
+        />
+      )}
+    </div>
+  )
+}
+
+LimitForm = forwardRef(LimitForm)
+
+const TaskBasicForm = props => {
+  const { onSubmit, formValues } = props
+  const [tipTime, setTipTime] = useState('')
+  const handleSubmit = e => {
+    e.preventDefault()
+    props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        // TODO: connet register api
+        console.log('Received values of form: ', values)
+        onSubmit(values)
+      }
+    })
+  }
+
+  const disabledDate = current => {
+    return current && current < moment().subtract('seconds', moment().seconds())
+  }
+
+  const handleDueTimeChange = _moment => {
+    if (!_moment) {
+      setTipTime('')
+    } else {
+      setTipTime(`任务将${_moment.fromNow(true)}后结束`)
+    }
+  }
+  const commonFields = [
+    {
+      type: 'title',
+      options: {
+        rules: [
+          {
+            min: 5,
+            max: 30,
+            required: true,
+            message: '输入5-30字的标题'
+          }
+        ]
+      }
+    },
+    {
+      type: 'isSurvey',
+      options: {
+        initialValue: false
+      }
+    },
+    {
+      type: 'dueTime',
+      options: {
+        rules: [
+          {
+            required: true,
+            message: '请输入截至日期'
+          }
+        ]
+      }
+    },
+    {
+      type: 'reward',
+      options: {
+        initialValue: 1
+      }
+    },
+    {
+      type: 'maximumParticipators',
+      options: {
+        initialValue: 1
+      }
+    },
+    {
+      type: 'limits',
+      options: {}
+    }
+  ]
+
+  commonFields.forEach(item => {
+    if (item.type in formValues) {
+      item.options.initialValue = formValues[item.type]
+    }
+  })
+
+  const { getFieldDecorator } = props.form
+  const commonDecorators = {}
+  commonFields.forEach(field => {
+    commonDecorators[field.type] = getFieldDecorator(field.type, field.options)
+  })
+  return (
+    <Card title="任务" className="task-card">
+      <Form onSubmit={handleSubmit} className="login-form">
+        <Form.Item label="标题">
+          {commonDecorators['title'](<Input type="text" />)}
+        </Form.Item>
+        <Form.Item label="任务类型">
+          {commonDecorators['isSurvey'](
+            <Radio.Group>
+              <Radio value={false}>普通任务</Radio>
+              <Radio value={true}>问卷</Radio>
+            </Radio.Group>
+          )}
+        </Form.Item>
+        <Form.Item label="截至时间">
+          {commonDecorators['dueTime'](
+            <DatePicker
+              format="YYYY-MM-DD HH:mm"
+              showTime={{
+                format: 'HH:mm'
+              }}
+              disabledDate={disabledDate}
+              onChange={handleDueTimeChange}
+            />
+          )}
+          {tipTime && <Alert message={tipTime} type="info" showIcon />}
+        </Form.Item>
+        <Form.Item label="悬赏">
+          {commonDecorators['reward'](<InputNumber min={1} suffix="金币" />)}
+        </Form.Item>
+        <Form.Item label="最多参与人数">
+          {commonDecorators['maximumParticipators'](<InputNumber min={1} />)}
+        </Form.Item>
+        <Form.Item label="额外限制">
+          {commonDecorators['limits'](<LimitForm />)}
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            继续填写
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  )
+}
+const WrappedTaskBasicForm = Form.create({ name: 'task_basic' })(TaskBasicForm)
+export default WrappedTaskBasicForm
