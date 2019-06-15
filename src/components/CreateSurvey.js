4df761785d87820dd7642894b87e0b436242d8da
Dmaxzj <260073361@qq.com>
2019-06-15 14:03:36 +0800

    添加 发布任务 组件

--- src/components/CreateSurvey.js
+++ src/components/CreateSurvey.js
@@ -9,14 +9,15 @@ import {
   Select,
   Button,
   Divider,
-  Input
+  Input,
+  message
 } from 'antd'
 
 const { Option } = Select
 
 export const SurveyList = props => {
   const { dataSource, removeItem } = props
-  const handleClick = (index) => {
+  const handleClick = index => {
     removeItem && removeItem(index)
   }
   return (
@@ -41,96 +42,173 @@ export const SurveyList = props => {
   )
 }
 
-const AddOption = props => {
-  const { prefix, onChange, validateStatus, help } = props
+const Choices = props => {
+  const { onChange, addChoiceValidate, choices } = props
   const [value, setValue] = useState('')
+  const [first, setFirst] = useState(true)
+  let [validateStatus, help] = first
+    ? ['success', '']
+    : addChoiceValidate(value.trim(), choices)
+  const handleAdd = () => {
+    if (validateStatus === 'success') {
+      onChange(choices.concat(value.trim()))
+      setValue('')
+      setFirst(true)
+    }
+  }
+  return (
+    <>
+      <p>选项: </p>
+      {choices.length !== 0 && (
+        <List
+          dataSource={choices}
+          renderItem={(item, index) => {
+            return (
+              <div>
+                {`选项${index + 1}: `}
+                <Checkbox checked={false}>{item}</Checkbox>
+              </div>
+            )
+          }}
+        />
+      )}
+      <AddOption
+        validateStatus={validateStatus}
+        help={help}
+        prefix={`选项${choices.length + 1}`}
+        value={value}
+        onChange={v => {
+          setFirst(false)
+          setValue(v)
+        }}
+        addButton={
+          <Button disabled={first} type="primary" onClick={handleAdd}>
+            新增选项
+          </Button>
+        }
+      />
+    </>
+  )
+}
+
+const AddOption = props => {
+  const { prefix, onChange, validateStatus, help, value, addButton } = props
 
   return (
     <Form.Item validateStatus={validateStatus} help={help}>
       <div className={{ display: 'flex' }}>
         {prefix}
-        <Input value={value} onChange={e => setValue(e.target.value)} />
-        <Button type="primary" onClick={() => onChange(value)}>
-          新增选项
-        </Button>
+        <Input value={value} onChange={e => onChange(e.target.value)} />
+        {addButton}
       </div>
     </Form.Item>
   )
 }
 
-const CreateQuestion = (props, ref) => {
+const QuestionLimit = props => {
+  const { isRequired, type, onRequiredChange, onTypeChange, types } = props
+  const getTypeDisplayName = type => {
+    const typeToZh = {
+      radio: '单选',
+      multyCheck: '多选',
+      text: '文本'
+    }
+    return typeToZh[type] || '文本'
+  }
+  return (
+    <div>
+      <label>问题类型: </label>
+      <Checkbox
+        checked={isRequired}
+        onChange={e => onRequiredChange(e.target.checked)}
+      >
+        必填
+      </Checkbox>
+      <Select defaultValue={type} onChange={v => onTypeChange(v)}>
+        {types.map(type => (
+          <Option key={type}>{getTypeDisplayName(type)}</Option>
+        ))}
+      </Select>
+    </div>
+  )
+}
+
+const TitleInput = props => {
+  const { value, onChange } = props
+  return (
+    <Form.Item label="标题">
+      <Input.TextArea
+        value={value}
+        autosize={{ minRows: 2, maxRows: 4 }}
+        maxLength="400"
+        onChange={e => {
+          const value = e.target.value
+          onChange(value)
+        }}
+      />
+    </Form.Item>
+  )
+}
+
+const CreateQuestion = props => {
   const { onCreate } = props
   const types = ['radio', 'multyCheck', 'text']
-  const [isOpen, setOpen] = useState(false)
-  const [questionData, setQuestionData] = useState({
+  const initData = {
     title: '',
     isRequired: true,
     type: types[0],
     options: []
-  })
+  }
+  const [isOpen, setOpen] = useState(false)
+  const [questionData, setQuestionData] = useState(initData)
 
-  const [validate, setValidate] = useState({
-    validateStatus: 'success',
-    help: ''
-  })
-
-  const [validateTitle, setValidateTitle] = useState({
-    validateStatus: 'success',
-    help: ''
-  })
-
-  const getTypeDisplayName = type => {
-    let name = ''
-    switch (type) {
-      case 'radio':
-        name = '单选'
-        break
-      case 'multyCheck':
-        name = '多选'
-        break
-      case 'text':
-        name = '文本'
-        break
-      default:
-        break
+  const validators = {
+    choiceItem: v => {
+      if (v) {
+        return ['success', '']
+      } else {
+        return ['error', '选项不能为空']
+      }
+    },
+    choiceRepeat: (v, vs) => {
+      if (!vs.includes(v)) {
+        return ['success', '']
+      } else {
+        return ['error', '选项已存在']
+      }
+    },
+    choiceAdd: (v, vs) => {
+      let help
+      ;[validators.choiceItem, validators.choiceRepeat].find(
+        validate => (help = validate(v, vs)[1])
+      )
+      const status = help ? 'error' : 'success'
+      return [status, help]
     }
-    return name
+  }
+
+  const handleChange = type => {
+    return value => {
+      setQuestionData({ ...questionData, [type]: value })
+    }
   }
 
   const handleOpenChange = () => {
     setOpen(!isOpen)
   }
 
-  const titleValidator = title => {
-    if (title && title.length >= 3) {
-      setValidateTitle({ validateStatus: 'success', help: '' })
-      return true
-    } else {
-      setValidateTitle({ validateStatus: 'error', help: '问题标题至少3个字' })
-      return false
-    }
-  }
-
   const handleAdd = () => {
-    if (titleValidator(questionData.title)) {
-      let flag = false
-      if (questionData.type !== 'text') {
-        if (questionData.options.length >= 1) {
-          flag = onCreate(questionData)
-        } else {
-          setValidate({ validateStatus: 'error', help: '选择题至少有一个选项' })
-        }
-      } else {
-        flag = onCreate(questionData)
-      }
-      if (flag) {
+    if (questionData.title.trim().length < 3) {
+      message.error('问题标题需要至少三个字')
+    } else if (
+      questionData.type !== 'text' &&
+      questionData.options.length < 1
+    ) {
+      message.error('选择题至少有一个选项')
+    } else {
+      if (onCreate(questionData)) {
+        setQuestionData(initData)
         setOpen(false)
-        setQuestionData({
-          title: '',
-          isRequired: true,
-          type: types[0],
-          options: []
-        })
       }
     }
   }
@@ -139,105 +217,9 @@ const CreateQuestion = (props, ref) => {
     setQuestionData({ type: value, options: [], isRequired: true, title: '' })
   }
 
-  const addChoiceItem = value => {
-    const optionName = value
-    const options = questionData.options || []
-    if (optionName.trim().length === 0) {
-      setValidate({ validateStatus: 'error', help: '选项不能为空' })
-    } else if (!options.includes(optionName)) {
-      setQuestionData({
-        ...questionData,
-        options: questionData.options.concat(optionName)
-      })
-      setValidate({ validateStatus: 'success', help: '' })
-    } else {
-      setValidate({ validateStatus: 'error', help: '选项已存在' })
-    }
-  }
-
-  const Choices = () => {
-    return (
-      <>
-        {'选项: \n'}
-        {questionData.options.length !== 0 && (
-          <List
-            dataSource={questionData.options}
-            renderItem={(item, index) => {
-              return (
-                <div>
-                  {`选项${index + 1}: `}{' '}
-                  <Checkbox checked={false}>{item}</Checkbox>
-                </div>
-              )
-            }}
-          />
-        )}
-        <AddOption
-          {...validate}
-          prefix={`选项${questionData.options.length + 1}`}
-          onChange={addChoiceItem}
-        />
-      </>
-    )
-  }
   const OpenButton = <Button onClick={handleOpenChange}>添加问题</Button>
   const CancleButton = <Button onClick={handleOpenChange}>取消</Button>
   const AddButton = <Button onClick={handleAdd}>添加问题</Button>
-
-  const TitleInput = props => {
-    const { value, onBlur, validateStatus, help } = props
-    const [formData, setFormData] = useState({
-      value: value,
-      validateStatus: validateStatus,
-      help: help
-    })
-    return (
-      <Form.Item
-        label="标题"
-        validateStatus={formData.validateStatus}
-        help={formData.help}
-      >
-        <Input.TextArea
-          value={formData.value}
-          autosize={{ minRows: 2, maxRows: 4 }}
-          maxLength="400"
-          onChange={e => {
-            const value = e.target.value
-            if (value && value.length >= 3) {
-              setFormData({ value, validateStatus: 'success', help: '' })
-            } else {
-              setFormData({
-                value,
-                validateStatus: 'error',
-                help: '问题标题至少3个字'
-              })
-            }
-          }}
-          onBlur={() => onBlur(formData.value)}
-        />
-      </Form.Item>
-    )
-  }
-  const QuestionLimit = () => {
-    return (
-      <div>
-        {'问题类型: '}
-        <Checkbox
-          checked={questionData.isRequired}
-          onChange={e =>
-            setQuestionData({ ...questionData, isRequired: e.target.checked })
-          }
-        >
-          必填
-        </Checkbox>
-        <Select defaultValue={questionData.type} onChange={handleSelectChange}>
-          {types.map(type => (
-            <Option key={type}>{getTypeDisplayName(type)}</Option>
-          ))}
-        </Select>
-      </div>
-    )
-  }
 
   return (
     <div>
@@ -247,14 +229,22 @@ const CreateQuestion = (props, ref) => {
         <>
           <TitleInput
             value={questionData.title}
-            {...validateTitle}
-            onBlur={title => {
-              titleValidator(title)
-              setQuestionData({ ...questionData, title })
-            }}
+            onChange={handleChange('title')}
           />
-          <QuestionLimit />
-          {questionData.type === 'text' ? null : <Choices />}
+          <QuestionLimit
+            isRequired={questionData.isRequired}
+            types={types}
+            type={questionData.type}
+            onRequiredChange={handleChange('isRequired')}
+            onTypeChange={handleSelectChange}
+          />
+          {questionData.type === 'text' ? null : (
+            <Choices
+              onChange={handleChange('options')}
+              addChoiceValidate={validators['choiceAdd']}
+              choices={questionData.options}
+            />
+          )}
           {AddButton}
           {CancleButton}
         </>
@@ -268,10 +258,13 @@ const CreateSurvey = props => {
 
   return (
     <Card title="问卷">
-      <SurveyList dataSource={surveyData} removeItem={(index) => onRemove(index)}/>
+      <SurveyList
+        dataSource={surveyData}
+        removeItem={index => onRemove(index)}
+      />
       <CreateQuestion onCreate={onChange} />
       <Button onClick={onPrev}>返回上步</Button>
-      <Button onClick={onSubmit}>完成问卷</Button>
+      <Button onClick={onSubmit} type="primary">完成问卷</Button>
     </Card>
   )
 }
