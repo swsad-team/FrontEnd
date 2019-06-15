6c55e72ab1c2c15151860b59a8a25b59ec4a8ac0
Dennis273 <dennisc695@gmail.com>
2019-06-15 13:16:41 +0800

    完成 TaskList 与 Filter 组件

---
+++ src/components/TaskFilter.jsx
@@ -0,0 +1,79 @@
+import { Checkbox, Select } from 'antd'
+
+import PropTypes from 'prop-types'
+import React from 'react'
+import styles from './TaskFilter.module.css'
+
+const Option = Select.Option
+
+function TaskFilter({
+  filters = [],
+  sorts = [],
+  selectedFilters = [],
+  selectedSort = null,
+  onChange
+}) {
+  const handleChange = ({ f = selectedFilters, s = selectedSort }) => {
+    onChange({
+      filters: f,
+      sort: s
+    })
+  }
+  const checkZone = (
+    <>
+      {filters.map((val, i) => (
+        <Checkbox
+          key={i}
+          checked={selectedFilters.includes(val)}
+          onChange={e => {
+            if (e.target.checked) {
+              handleChange({
+                f: selectedFilters.push(val)
+              })
+            } else {
+              handleChange({
+                f: selectedFilters.filter(v => v !== val)
+              })
+            }
+          }}
+        >
+          {val}
+        </Checkbox>
+      ))}
+    </>
+  )
+  const selector = (
+    <div>
+      排序：
+      <Select
+        className={styles.selector}
+        onChange={val => {
+          handleChange({
+            s: val
+          })
+        }}
+      >
+        {sorts.map((val, i) => (
+          <Option key={i} value={val}>
+            {val}
+          </Option>
+        ))}
+      </Select>
+    </div>
+  )
+  return (
+    <div className={styles.content}>
+      {filters.length !== 0 ? checkZone : null}
+      {sorts.length !== 0 ? selector : null}
+    </div>
+  )
+}
+
+TaskFilter.propTypes = {
+  filters: PropTypes.arrayOf(PropTypes.string),
+  sorts: PropTypes.arrayOf(PropTypes.string),
+  selectedFilters: PropTypes.arrayOf(PropTypes.string),
+  selectedSort: PropTypes.arrayOf(PropTypes.string)
+}
+
+export default TaskFilter
