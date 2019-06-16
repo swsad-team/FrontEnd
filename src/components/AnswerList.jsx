f493bab9eaabaff85c7dfed2d317c1063d64fc15
Dmaxzj <Dmaxzj@users.noreply.github.com>
2019-06-16 22:58:45 +0800

    添加: 问卷 按问题显示

---
+++ src/components/AnswerList.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { List, Collapse, Radio, Checkbox } from 'antd'
+import styles from './AnswerList.module.css'
+
+const Panel = Collapse.Panel
+
+function AnswerList ({ survey, answers }) {
+  console.log(survey, answers)
+  const [keys, setKeys] = useState([])
+
+  const toItem = (question, answer) => {
+    const type = question.type
+    if (type === 'text') {
+      return <p>{answer}</p>
+    } else if (type === 'radio') {
+      return <Radio.Group value={answer}>
+        {question.options.map(item => <Radio key={item} value={item}>{item}</Radio>)}
+      </Radio.Group>
+    } else {
+      return <Checkbox.Group options={question.options} value={answer} />
+    }
+  }
+
+  const getAnswerByIndex = index => {
+    return (
+      <List
+        dataSource={answers}
+        pagination={{
+          pageSize: 5
+        }}
+        renderItem={item => {
+          return (
+            <List.Item>
+              {String(item.content[index])
+                ? toItem(survey[index], item.content[index])
+                : '未填写'}
+              <span className={styles.author}>{'填写人: 匿名'}</span>
+            </List.Item>
+          )
+        }}
+      />
+    )
+  }
+
+  const handleChange = key => {
+    setKeys(key)
+  }
+
+  const Header = ({ item }) => {
+    return <h1 className={styles.header}>{item.title}</h1>
+  }
+
+  return (
+    <Collapse onChange={handleChange} className={styles.answersContainer}>
+      {survey.map((item, index) => (
+        <Panel header={<Header item={item} />} key={item.title}>
+          {keys.includes(item.title) && getAnswerByIndex(index)}
+        </Panel>
+      ))}
+    </Collapse>
+  )
+}
+
+export default AnswerList
