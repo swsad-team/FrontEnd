69e955aaaef3ef7cdd4e88a31101cb2781dbd06c
Dennis273 <dennisc695@gmail.com>
2019-06-25 21:44:18 +0800

    fix: 修正 task 列表问题

--- src/components/AllTaskList.jsx
+++ src/components/AllTaskList.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import TaskListContainer from './TaskListContainer'
-import { taskApi } from '../apis';
-
+import { taskApi } from '../apis'
 function AllTaskList() {
-  const getTaskFunc = async (filters, sorter, page) => {
-    __tasks__ = await taskApi.getAllTasks(
-      page, 
-      10, 
-      filters.map(val => filtersOption[val]), 
-      sortersOption[sorter]
+  const getTaskFunc = async (filters, sort, page) => {
+    return taskApi.getAllValidTasks(
+      page,
+      10,
+      filters.map(val => filtersOption[val]),
+      sortersOption[sort]
     )
-    return __tasks__
   }
-
   return (
     <TaskListContainer
       optFilters={Object.keys(filtersOption)}
@@ -22,31 +19,16 @@ function AllTaskList() {
   )
 }
 
-// const __task__ = {
-//   tid: 1,
-//   publisher: 2,
-//   title: '买奶茶',
-//   description: '买一fasfsfadsfasfdfafadsfadafdfafasf杯奶茶',
-//   isQuestionnaire: 'false',
-//   startTime: 1560517928893,
-//   endTime: 1560577998893,
-//   reward: 2,
-//   coinPool: 40,
-//   numOfPeople: 20,
-//   participants: [1, 2, 3, 4, 5, 6],
-//   finishers: []
-// }
-
-let __tasks__ = []
-
-const filtersOption =  {
-  个人发布: 'personal',
-  组织发布: 'organizational',
-  可参加: 'participable'
-}
-const sortersOption =  {
+const sortersOption = {
   悬赏金: 'coin',
   时间: 'time',
+}
+const filtersOption = {
+  个人发布: 'personal',
+  组织发布: 'organizational',
+  可参加: 'participable',
+  问卷: 'questionnaire',
+  任务: 'mission',
 }
 
 export default AllTaskList
