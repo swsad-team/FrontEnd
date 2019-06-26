032118dd2a99e4f2ca282826fb20a70c54a7df79
Dmaxzj <260073361@qq.com>
2019-06-26 19:11:25 +0800

    fix: 移除未使用变量

--- src/components/AnswerList.jsx
+++ src/components/AnswerList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { List, Collapse, Radio, Checkbox } from 'antd'
 import styles from './AnswerList.module.css'
 import { UserContext } from '../context'
